391629e7c882f9eb5e418c45fa430a96551e300a
vardhaman <vardhaman@htop.info>
2019-11-15 22:33:37 -0800

    rebuilding site Fri Nov 15 22:33:37 PST 2019

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,10 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
+hugo -t spectre-pixel # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Add changes to git.
-git add .
+git add --all :/
 
 # Commit changes.
 msg="rebuilding site $(date)"
